bc9da89334f170b5707903ef115d7c0d0369f354
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-21 23:06:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,30 +16,6 @@ client.on("message", message => {
 });
 
 
- client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-        message.channel.createInvite({
-        thing: true,
-        maxUses: 5,
-        maxAge: 3600,
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-          .setDescription("تم أرسال الرابط برسالة خاصة")
-           .setAuthor(client.user.username, client.user.avatarURL)
-                 .setAuthor(client.user.username, client.user.avatarURL)
-                .setFooter('طلب بواسطة: ' + message.author.tag)
-
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        
-    .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 5 **")
-      message.author.sendEmbed(Embed11)
-    }
-
 client.on('ready', () => {
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : pretty`);
