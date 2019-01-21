b5f447a77225e27f27e34ecb982f84ec6132cdd1
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-21 23:02:47 +0200

    Update bc.js

--- bc.js
+++ bc.js
@@ -1,27 +1,6 @@
-﻿const Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "$";
-
-client.on('ready', function(){
-    var ms = 10000 ;
-    var setGame = [' رابط','الشيخ بريتي','One Server !',' ! الشيخ بريتي شخصيآ#8888',' يـارب'];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/one`);
-    }, ms);
-  console.log('---------------');
-  console.log('GaMerZ -Bot Is Online')
-  console.log('---------------')
-});
-
 client.on("message", message => {
 
             if (message.content.startsWith(prefix + "bc")) {
@@ -60,6 +39,17 @@ client.on("message", message => {
     .setDescription("** مدة الرابط : ساعه | عدد استخدامات الرابط : 5 **")
       message.author.sendEmbed(Embed11)
     }
+
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Desert Bot- Script By : pretty`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : pretty ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`الشيخ بريتي`,"http://twitch.tv/S-F")
+client.user.setStatus("dnd")
+});
 
 
 
