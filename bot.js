c6938d54bb22f28c907b0c69cf021bfa5f2ecf42
pretty3831 <46170310+pretty3831@users.noreply.github.com>
2019-01-21 23:09:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,18 +16,25 @@ client.on("message", message => {
 });
 
 
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : pretty`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : pretty ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`الشيخ بريتي`,"http://twitch.tv/S-F")
-client.user.setStatus("dnd")
+client.on('ready', function(){
+    var ms = 10000 ;
+    var setGame = ['رابط','الشيخ بريتي','One Server !',' ! have fun','! الشيخ بريتي شخصيآ#8888'];
+    var i = -1;
+    var j = 0;
+    setInterval(function (){
+        if( i == -1 ){
+            j = 1;
+        }
+        if( i == (setGame.length)-1 ){
+            j = -1;
+        }
+        i = i+j;
+        client.user.setGame(setGame[i],`http://www.twitch.tv/one`);
+    }, ms);
+  console.log('---------------');
+  console.log('GaMerZ -Bot Is Online')
+  console.log('---------------')
 });
-
-
 
 
 client.login("NTM2OTkxMzMzMTUyNDU2NzI1.DyfA2Q.hLjrNRaz3swRUl4iXuPBbs69-zc");
